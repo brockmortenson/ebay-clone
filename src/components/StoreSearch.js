81375ca36a241ebde82fe93acb4138afc7464bed
Brock Mortenson <mortensonbrock@gmail.com>
2021-07-23 20:58:42 -0600

    Was able to get the search bar working. Styled the results that are being returned in a sort of drop down below the search bar. Added a loading icon that replaces the word 'Search' in the search button as the data for the search is being retrieved. I am having some trouble displaying an error when no search results are found because depending on what I do it will either display the error 20 times, once for each item, or it will display the error for items that the search did not work for while it works for other items. I will figure that out later.

--- src/components/StoreSearch.js
+++ src/components/StoreSearch.js
@@ -1,29 +1,209 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import '../styles/storeSearch.css';
 
 function StoreSearch() {
     // SearchBar State
-    // const [ name, setName ] = useState([]);
-    // const [ selectedFilter, setFilter ] = useState('none');
-    // const [ searchParams, setSearchParams ] = useState('');
-    // const [ results, setResults ] = useState(null);
+    const [ selectedFilter, setFilter ] = useState('all');
+    const [ searchParams, setSearchParams ] = useState('');
+    const [ results, setResults ] = useState(null);
+    const [ searchFail, setSearchFail ] = useState(true);
+
+    const [ data, setData ] = useState(false);
+
+    const [ loading, setLoading ] = useState(false);
+
+    const history = useHistory();
 
     const handleItemSearch = async () => {
+        setLoading(true)
         try {
             await axios
                     .get('https://fakestoreapi.com/products')
                     .then(res => {
-                        console.log(res.data)
-                        
+                        const mapped = res.data.map((item) => {
+                            if (item.title.toLowerCase().includes(searchParams)) {
+                                setSearchFail(false)
+                                setData(true)
+                                return (
+                                    <Link
+                                        key={item.id}
+                                        to={`/ProductView/${item.id}`}
+                                        style={{ textDecoration: 'none' }}
+                                    >
+                                        <div className='item-search' onClick={() => setResults(null)}>
+                                            <img src={item.image} alt='products1' />
+                                            <p>{item.title}</p>
+                                            <div>${item.price}</div>
+                                        </div>
+                                    </Link>
+                                );
+                            } 
+                            // create classname with state and change
+                            // classname when item.includes can't be found
+                        })
+                        // console.log(results)
+                        setLoading(false)
+                        setResults(mapped)
                     })
         } catch (err) {
             console.log(err);
         }
     }
 
-    const handleSubmit = () => {
+    const handleElecSearch = async () => {
+        setLoading(true)
+        try {
+            await axios
+                    .get(`https://fakestoreapi.com/products/category/electronics`)
+                    .then(res => {
+                        const mapped = res.data.map((item) => {
+                            if (item.title.toLowerCase().includes(searchParams)) {
+                                setSearchFail(false)
+                                return (
+                                    <Link
+                                        key={item.id}
+                                        to={`/ProductView/${item.id}`}
+                                        style={{ textDecoration: 'none' }}
+                                    >
+                                        <div className='item-search' onClick={() => setResults(null)}>
+                                            <img src={item.image} alt='products1' />
+                                            <p>{item.title}</p>
+                                            <div>${item.price}</div>
+                                        </div>
+                                    </Link>
+                                );
+                            } else {
+                                setSearchFail(true)
+                            }
+                        })
+                        setLoading(false)
+                        setResults(mapped)
+                    })
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
+    const handleMenSearch = async () => {
+        setLoading(true)
+        try {
+            await axios
+                    .get(`https://fakestoreapi.com/products/category/men's%20clothing`)
+                    .then(res => {
+                        const mapped = res.data.map((item) => {
+                            if (item.title.toLowerCase().includes(searchParams)) {
+                                setSearchFail(false)
+                                return (
+                                    <Link
+                                        key={item.id}
+                                        to={`/ProductView/${item.id}`}
+                                        style={{ textDecoration: 'none' }}
+                                    >
+                                        <div className='item-search' onClick={() => setResults(null)}>
+                                            <img src={item.image} alt='products1' />
+                                            <p>{item.title}</p>
+                                            <div>${item.price}</div>
+                                        </div>
+                                    </Link>
+                                );
+                            } else {
+                                setSearchFail(true)
+                            }
+                        })
+                        setLoading(false)
+                        setResults(mapped)
+                    })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const handleWomenSearch = async () => {
+        setLoading(true)
+        try {
+            await axios
+                    .get(`https://fakestoreapi.com/products/category/women's%20clothing`)
+                    .then(res => {
+                        const mapped = res.data.map((item) => {
+                            if (item.title.toLowerCase().includes(searchParams)) {
+                                setSearchFail(false)
+                                return (
+                                    <Link
+                                        key={item.id}
+                                        to={`/ProductView/${item.id}`}
+                                        style={{ textDecoration: 'none' }}
+                                    >
+                                        <div className='item-search' onClick={() => setResults(null)}>
+                                            <img src={item.image} alt='products1' />
+                                            <p>{item.title}</p>
+                                            <div>${item.price}</div>
+                                        </div>
+                                    </Link>
+                                );
+                            } else {
+                                setSearchFail(true)
+                            }
+                        })
+                        setLoading(false)
+                        setResults(mapped)
+                    })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const handleJewelrySearch = async () => {
+        setLoading(true)
+        try {
+            await axios
+                    .get(`https://fakestoreapi.com/products/category/jewelery`)
+                    .then(res => {
+                        const mapped = res.data.map((item) => {
+                            if (item.title.toLowerCase().includes(searchParams)) {
+                                setSearchFail(false)
+                                return (
+                                    <Link
+                                        key={item.id}
+                                        to={`/ProductView/${item.id}`}
+                                        style={{ textDecoration: 'none' }}
+                                    >
+                                        <div className='item-search' onClick={() => setResults(null)}>
+                                            <img src={item.image} alt='products1' />
+                                            <p>{item.title}</p>
+                                            <div>${item.price}</div>
+                                        </div>
+                                    </Link>
+                                );
+                            } else {
+                                setSearchFail(true)
+                            }
+                        })
+                        setLoading(false)
+                        setResults(mapped)
+                    })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (searchParams === '') {
+            setResults(null);
+            return results;
+        } else if (selectedFilter === 'all') {
+            handleItemSearch();
+        } else if (selectedFilter === 'men') {
+            handleMenSearch();
+        } else if (selectedFilter === 'electronics') {
+            handleElecSearch();
+        } else if (selectedFilter === 'women') {
+            handleWomenSearch();
+        } else if (selectedFilter === 'jewelry') {
+            handleJewelrySearch();
+        }
     }
     
     return (
@@ -31,14 +211,16 @@ function StoreSearch() {
             <form onSubmit={handleSubmit}>
                 <select
                     name='search-parameter'
-                    defaultValue={'none'}
+                    defaultValue={'all'}
+                    onChange={(e) => setFilter(e.target.value)}
                 >
                     <option
                         disabled
-                        value='none'
+                        value='all'
                     >
                         Category
                     </option>
+                    <option value='all'>All</option>
                     <option value='electronics'>Electronics</option>
                     <option value='men'>Men's</option>
                     <option value='women'>Women's</option>
@@ -47,10 +229,38 @@ function StoreSearch() {
                 <input
                     placeholder='Search for anything'
                     type='text'
-                    onChange={handleItemSearch}
+                    onChange={(e) => setSearchParams(e.target.value)}
                 />
-                <button style={{textDecoration: 'none'}} type='submit'>Search</button>
+                <button
+                    style={{textDecoration: 'none'}}
+                    type='submit'
+                    onClick={() => history.push('/')}
+                >
+                {
+                    loading
+                    ?
+                    <div className='load-search'><div></div><div></div><div></div><div></div></div>
+                    :
+                    <h3>Search</h3>
+                }
+                </button>
             </form>
+            {
+                !loading && results !== null
+                ?
+                <div className='results' onMouseLeave={() => setResults(null)}>
+                    {
+                        searchFail
+                        ?
+                        <div>No results were found that match your search : (</div>
+                        :
+                        null
+                    }
+                    {results}
+                </div>
+                :
+                null
+            }
         </div>
     );
 }
